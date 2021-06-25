bd5457089713f9f1cbfeefb359ca9f5b0d36ec72
dbto1970 <donbartojunior@gmail.com>
2021-06-25 15:38:51 -0400

    added some elements to Member Page, including some js for time date and message/trade mockup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,34 @@
 $(function() {
+    // Time and Date
+// make new Date Object and use function to update time while on page
+function updateTime() {    
+    const today = new Date();
+    var displayTime = today.toLocaleTimeString();
+    var updateTimeDisplay = setInterval(dateTimeClock, 1000);
     
+    function dateTimeClock() {
+        var upDate = new Date();
+        var displayTime = upDate.toLocaleTimeString();
+        document.getElementById("date-time").innerHTML = today.toDateString() + "<br />" + displayTime;
+    }
+}
+updateTime();
+
+
+// return values for messages and trades
+
+let messages = '0';
+let trades = '0';
+fakeData();
+
+function fakeData() {
+    for (i = 0; i < 10; i++) {
+        messages = i;
+        trades = i + 1;
+        $("#messages-waiting").html(messages);
+        $("#trades-pending").html(trades);
+    }
+
+}
+
 });
