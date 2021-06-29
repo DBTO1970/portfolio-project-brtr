fba1863127db61c7d0c2b0c12780f194770e15b5
dbto1970 <donbartojunior@gmail.com>
2021-06-29 09:13:44 -0400

    fixed js time-date and expanded function

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,32 +3,39 @@ $(function() {
 // make new Date Object and use function to update time while on page
 function updateTime() {    
     const today = new Date();
-    var displayTime = today.toLocaleTimeString();
-    var updateTimeDisplay = setInterval(dateTimeClock, 1000);
+    let displayTime = today.toLocaleTimeString();
+    let updateTimeDisplay = setInterval(dateTimeClock, 1000);
     
     function dateTimeClock() {
-        var upDate = new Date();
-        var displayTime = upDate.toLocaleTimeString();
+        let upDate = new Date();
+        displayTime = upDate.toLocaleTimeString();
         document.getElementById("date-time").innerHTML = today.toDateString() + "<br />" + displayTime;
     }
 }
-updateTime();
+
+if ( $('.member-header').length > 0 || $('.guest-header').length > 0) {
+    $('.date-time').removeClass('d-none');
+    updateTime();
+};
 
 
-// return values for messages and trades
-
-// let messages = '0';
-// let trades = '0';
 
 
-// function fakeData() {
-//     for (i = 0; i < 10; i++) {
-//         messages = i;
-//         trades = i + 1;
-//         $("#messages-waiting").html(messages);
-//         $("#trades-pending").html(trades);
-//     }
+//return values for messages and trades
 
-// }
-// fakeData();
+let messages = '0';
+let trades = '0';
+
+
+function fakeData() {
+    for (i = 0; i < 10; i++) {
+        messages = i;
+        trades = i + 1;
+        $("#messages-waiting").html(messages);
+        $("#trades-pending").html(trades);
+    }
+
+}
+fakeData();
 });
+
