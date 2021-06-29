8952c534535c8d0e95d62c04ee0b1ab81131513e
dbto1970 <donbartojunior@gmail.com>
2021-06-29 10:16:01 -0400

    fixed js for all page build

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,46 @@
 $(function() {
     // Time and Date
 // make new Date Object and use function to update time while on page
-function updateTime() {    
-    const today = new Date();
-    let displayTime = today.toLocaleTimeString();
-    let updateTimeDisplay = setInterval(dateTimeClock, 1000);
-    
-    function dateTimeClock() {
-        let upDate = new Date();
-        displayTime = upDate.toLocaleTimeString();
-        document.getElementById("date-time").innerHTML = today.toDateString() + "<br />" + displayTime;
+    function updateTime() {    
+        const today = new Date();
+        var displayTime = today.toLocaleTimeString();
+        setInterval(dateTimeClock, 1000);
+        
+        function dateTimeClock() {
+            upDate = new Date();
+            displayTime = upDate.toLocaleTimeString();
+            document.getElementById("date-time").innerHTML = today.toDateString() + "<br />" + displayTime;
+        }
     }
-}
 
-if ( $('.member-header').length > 0 || $('.guest-header').length > 0) {
-    $('.date-time').removeClass('d-none');
-    updateTime();
-};
+    if ( $('.member-header').length > 0 || $('.guest-header').length > 0) {
+        $('.date-time').removeClass('d-none');
+        updateTime();
+    }
 
 
 
 
 //return values for messages and trades
 
-let messages = '0';
-let trades = '0';
+    let messages = '0';
+    let trades = '0';
 
 
-function fakeData() {
-    for (i = 0; i < 10; i++) {
-        messages = i;
-        trades = i + 1;
-        $("#messages-waiting").html(messages);
-        $("#trades-pending").html(trades);
+    function fakeData() {
+        for (i = 0; i < 10; i++) {
+            messages = i;
+            trades = i + 1;
+            $("#messages-waiting").html(messages);
+            $("#trades-pending").html(trades);
+        }
+
     }
 
-}
-fakeData();
+    if ( $('.member-header').length > 0) {
+       
+        fakeData();
+    }
+    
 });
 
