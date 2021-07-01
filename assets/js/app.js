6c1146dc59b172bfd4f07a1e16d5757efac84ece
dbto1970 <donbartojunior@gmail.com>
2021-07-01 15:34:29 -0400

    added datepicker and js, all pages through html validation.

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,7 +18,21 @@ $(function() {
         updateTime();
     }
 
-    // trade ticker
+// Date Time Picker  Documentation https://demos.creative-tim.com/material-kit/docs/2.1/plugins/datetimepicker.html
+
+$('.datetimepicker').datetimepicker({
+    icons: {
+      time: "fa fa-clock-o",
+      date: "fa fa-calendar",
+      up: "fa fa-chevron-up",
+      down: "fa fa-chevron-down",
+      previous: 'fa fa-chevron-left',
+      next: 'fa fa-chevron-right',
+      today: 'fa fa-screenshot',
+      clear: 'fa fa-trash',
+      close: 'fa fa-remove'
+    }
+  });
 
 
     
